14cabe84694706fb2edc8e94cff1be6ddb16e6bc
KimSuHyeon <sh975@naver.com>
2019-07-15 14:02:16 +0900

    MainActivity에 버튼 리스너 만듬

--- app/src/main/java/com/examplecho2/reservationswu/MainActivity.java
+++ app/src/main/java/com/examplecho2/reservationswu/MainActivity.java
@@ -3,6 +3,7 @@ package com.examplecho2.reservationswu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonGoogleSignIn=findViewById(R.id.btnGoogleSignIn);
-        
+
     }
 }
+
+
+    } // end onCreate
+
+    private View.OnClickListener mClicks = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btnGoogleSignIn:
+                    break;
+            }
+        }
+    };
+
+} // end class
