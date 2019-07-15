c900372918904ed38f4a25867eef220774a2f184
Leeyeojin <97jyl0109@gmail.com>
2019-07-15 14:59:11 +0900

    빈 생성

--- app/src/main/java/com/examplecho2/reservationswu/JoinActivity.java
+++ app/src/main/java/com/examplecho2/reservationswu/JoinActivity.java
@@ -1,18 +1,216 @@
 package com.examplecho2.reservationswu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class JoinActivity extends AppCompatActivity {
-    //학생증 사진
-    //학번
-    //전화번호 추가적으로 입력
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class JoinActivity extends AppCompatActivity { //학생증 사진, 전화번호를 입력받아 제출하는 액티비
+    public static final String STORAGE_DB_URL="";//firebase url
+
+    private ImageView mImgID;//학생증 사진
+    private EditText mEdtPhone;//전화번호
+    private EditText mEdtName;//학생 이름
+
+    //사진이 저장도니 경로 - onActivityResult()로부터 받는 데이터
+    private Uri mCaptureUri;
+    //사진이 저장된 단말기상의 실제 경로
+    public String mPhotoPath;
+    //startActivityForResult() 에 넘겨주는 값, 이 값이 나중에 onActivityResult()로 돌아와서
+    //내가 던진값인지를 구별할 때 사용하는 상수이다.
+    public static final int REQUEST_IMAGE_CAPTURE = 200;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
-        //시험
+
+        //카메라를 사용하기 위한 퍼미션을 요청한다.
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+        }, 0);
+
+        mImgID=findViewById(R.id.imgID);
+        mEdtPhone=findViewById(R.id.EdtPhone);
+        mEdtName=findViewById(R.id.EdtName);
+
+        mImgID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+
+    }//end Oncreate
+    private void takePicture() {
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mCaptureUri = Uri.fromFile( getOutPutMediaFile() );
+        } else {
+            mCaptureUri = FileProvider.getUriForFile(this,
+                    "com.example.guru2classexam", getOutPutMediaFile());
+        }
+
+        i.putExtra(MediaStore.EXTRA_OUTPUT, mCaptureUri);
+
+        //내가 원하는 액티비티로 이동하고, 그 액티비티가 종료되면 (finish되면)
+        //다시금 나의 액티비티의 onActivityResult() 메서드가 호출되는 구조이다.
+        //내가 어떤 데이터를 받고 싶을때 상대 액티비티를 호출해주고 그 액티비티에서
+        //호출한 나의 액티비티로 데이터를 넘겨주는 구조이다. 이때 호출되는 메서드가
+        //onActivityResult() 메서드 이다.
+        startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
+
+    }
+    private File getOutPutMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "cameraDemo");
+        if(!mediaStorageDir.exists()) {
+            if(!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File file = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+        mPhotoPath = file.getAbsolutePath();
+
+        return file;
+    }
+    private void sendPicture() {
+        Bitmap bitmap = BitmapFactory.decodeFile(mPhotoPath);
+        Bitmap resizedBmp = getResizedBitmap(bitmap, 4, 100, 100);
+
+        bitmap.recycle();
+
+        //사진이 캡쳐되서 들어오면 뒤집어져 있다. 이애를 다시 원상복구 시킨다.
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(mPhotoPath);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        int exifOrientation;
+        int exifDegree;
+        if(exif != null) {
+            exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+            exifDegree = exifOrientToDegree(exifOrientation);
+        } else {
+            exifDegree = 0;
+        }
+        Bitmap rotatedBmp = roate(resizedBmp, exifDegree);
+        mImgID.setImageBitmap( rotatedBmp );
+        //줄어든 이미지를 다시 저장한다
+        saveBitmapToFileCache(resizedBmp, mPhotoPath);
+
+        Toast.makeText(this, "사진경로: " + mPhotoPath, Toast.LENGTH_LONG).show();
+    }
+
+    private void saveBitmapToFileCache(Bitmap bitmap, String strFilePath) {
+
+        File fileCacheItem = new File(strFilePath);
+        OutputStream out = null;
+
+        try
+        {
+            fileCacheItem.createNewFile();
+            out = new FileOutputStream(fileCacheItem);
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                out.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private int exifOrientToDegree(int exifOrientation) {
+        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        }
+        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        }
+        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    private Bitmap roate(Bitmap bmp, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(),
+                matrix, true);
+    }
+
+    //비트맵의 사이즈를 줄여준다.
+    public static Bitmap getResizedBitmap(Bitmap srcBmp, int size, int width, int height){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = size;
+        Bitmap resized = Bitmap.createScaledBitmap(srcBmp, width, height, true);
+        return resized;
+    }
+
+    public static Bitmap getResizedBitmap(Resources resources, int id, int size, int width, int height){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = size;
+        Bitmap src = BitmapFactory.decodeResource(resources, id, options);
+        Bitmap resized = Bitmap.createScaledBitmap(src, width, height, true);
+        return resized;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //카메라로부터 오는 데이터를 취득한다.
+        if(resultCode == RESULT_OK) {
+            if(requestCode == REQUEST_IMAGE_CAPTURE) {
+                sendPicture();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/examplecho2/reservationswu/bean/ReservationBean.java
@@ -0,0 +1,8 @@
+package com.examplecho2.reservationswu.bean;
+
+public class ReservationBean {
+    public String date;//일자
+    public String building;//건물
+    public String room;//호실
+    public String time;//시간
+}
